3d508aa22f975e7dccc9f12b8166605857b84aa1
st8324 <stajun@naver.com>
2019-08-14 14:01:34 +0900

    DB 연동(Mybatis)

--- src/main/java/kr/green/spring/HomeController.java
+++ src/main/java/kr/green/spring/HomeController.java
@@ -1,18 +1,11 @@
 package kr.green.spring;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import javax.activation.CommandMap;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import kr.green.spring.service.MemberService;
 
 /**
  * Handles requests for the application home page.
@@ -20,7 +13,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@Autowired
+	MemberService memberService;
+	
+	@RequestMapping(value="/")
+	public ModelAndView main(ModelAndView mv) throws Exception{
+		mv.setViewName("/main/home");
+		mv.addObject("setHeader", "타일즈테스트");
+		System.out.println(memberService.getEmail("12345678"));
+		return mv;
+	}
 	
 	@RequestMapping(value= {"/main/home","/home.do"})
 	public ModelAndView openTilesView(ModelAndView mv) throws Exception{
@@ -28,4 +30,5 @@ public class HomeController {
 		mv.addObject("setHeader", "타일즈테스트");
 		return mv;
 	}
+	
 }
